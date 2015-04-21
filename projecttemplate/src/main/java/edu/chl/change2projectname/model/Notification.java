860915bfb91308306395aafef6c5277a2beb313e
johanbenm <johanbenmohammad@outlook.com>
2015-04-21 12:21:38 +0200

    Notification.java

---
+++ projecttemplate/src/main/java/edu/chl/change2projectname/model/Notification.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.chl.change2projectname.model;
+import java.util.Date;
+
+
+/**
+ *
+ * @author johan
+ */
+
+
+/* For now: The notification class includes all of the methods from the Alarm
+   class, since Alarm is an abstract class. For now: This class does not have
+   any unique method.
+*/
+public class Notification extends Alarm {
+    
+    @Override
+    public void setAlarm(Date time){
+        this.alarmTime = time;
+    }
+    
+    @Override
+    public Date getAlarm(){
+        return alarmTime;
+    }
+    
+    @Override
+    public void setAlarmName(String name){
+        this.name = name;
+    }
+    
+    @Override
+    public String getAlarmName(){
+        return name;
+    }
+    
+    @Override
+    public void setDescName(String desc){
+        this.description = desc;
+    }
+    
+    @Override
+    public String getDescName(){
+        return name;
+    }
+    
+    
+}
